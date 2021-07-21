2e5854624394931e7c9f1a9ada49771e701a8530
Suriba <SuribaCN@outlook.com>
2021-07-22 03:28:54 +0800

    重构GunBase，仍需做伤害判定。

--- Source/A/GunBase.cpp
+++ Source/A/GunBase.cpp
@@ -3,16 +3,62 @@
 
 #include "GunBase.h"
 
+
+#include "CollisionDebugDrawingPublic.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
 // Sets default values
 AGunBase::AGunBase()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	Model = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Model"));
+	MuzzleEmitter = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MuzzleEmitter"));
+	MuzzleEmitter->SetupAttachment(Model);
+	WeaponVFSEmitter = CreateDefaultSubobject<UParticleSystemComponent> (TEXT("WeaponVFSEmitter"));
+	WeaponVFSEmitter->SetupAttachment(Model);
+	Muzzle = CreateDefaultSubobject<USceneComponent>(TEXT("Muzzle"));
+	Muzzle->SetupAttachment(Model);
+	Muzzle->SetRelativeLocation(FVector(0,0,0));
+	Rail1 = CreateDefaultSubobject<USceneComponent>(TEXT("Rail1"));
+	Rail1->SetupAttachment(Model);
+	Rail2 = CreateDefaultSubobject<USceneComponent>(TEXT("Rail2"));
+	Rail2->SetupAttachment(Model);
+	Rail3 = CreateDefaultSubobject<USceneComponent>(TEXT("Rail3"));
+	Rail3->SetupAttachment(Model);
+	Rail4 = CreateDefaultSubobject<USceneComponent>(TEXT("Rail4"));
+	Rail4->SetupAttachment(Model);
 }
 
-void AGunBase::shoot_Implementation(FVector StartLocation,FVector EndLocation)
+void AGunBase::shoot()
 {
+	if(GetWorld()->GetTimeSeconds()-AllowShootTime>=0)
+	{
+	UE_LOG(LogTemp,Display,TEXT("%d"),RoundRemain)
+	if(RoundRemain>0)
+	{
+		if (true)//todo 根据武器发射物类型决定
+		{
+			TArray<struct FHitResult> OutHits;
+			FVector StartPosition=Muzzle->GetComponentLocation();
+			UE_LOG(LogTemp,Display,TEXT("SSS:%s"),*StartPosition.ToString())
+			FVector EndPosition=Muzzle->GetComponentLocation()+Range*Muzzle->GetForwardVector();
+			//UKismetSystemLibrary::LineTraceMulti(GetWorld(),Muzzle->GetComponentLocation(),Muzzle->GetComponentLocation()+Range*Muzzle->GetForwardVector(),ETraceTypeQuery::,0,NULL,EDrawDebugTrace::ForDuration,OutHits,1);
+			GetWorld()->LineTraceMultiByChannel(OutHits,StartPosition,EndPosition,ECollisionChannel::ECC_Visibility);
+            //MuzzleEmitter->Activate();
+			UGameplayStatics::SpawnEmitterAtLocation(GetWorld(),GunFireEmitter,StartPosition);
+			RoundRemain--;
+			UE_LOG(LogTemp,Display,TEXT("%d"),RoundRemain)
+			AllowShootTime=GetWorld()->GetTimeSeconds()+60/RPM;
+		}
+	}
+    else
+    {
+    	UE_LOG(LogTemp,Display,TEXT("No Ammo"))
+    }
+	}
 	
 }
 
@@ -29,13 +75,10 @@ FShootDirectionStruct AGunBase::GetShootDirection_Implementation()
 void AGunBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AGunBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
-
--- Source/A/GunBase.h
+++ Source/A/GunBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
 #include "GunBase.generated.h"
 
 
@@ -18,6 +19,15 @@ enum class EGunType:uint8
     Launcher UMETA(DisplayName="发射器"),
     Special UMETA(DisplayName="特殊装备 "),
 };
+
+UENUM(BlueprintType)
+enum class FireMode:uint8
+{
+    Auto UMETA(DisplayName="全自动"),
+    SemiAuto UMETA(DisplayName="半自动"),
+    Safe UMETA(DisplayName="保险"),
+};
+
 
 USTRUCT(BlueprintType)
 struct FShootDirectionStruct
@@ -36,25 +46,79 @@ class A_API AGunBase : public AActor
 public:
     // Sets default values for this actor's properties
     AGunBase();
-
+    /////////////////////////////////////武器基础属性///////////////////////////////////////////////////
     //用来决定枪支类型 蓝图可见
     UPROPERTY(BlueprintReadWrite,EditAnywhere)
     EGunType GunType;
-
-    //射击的顶层实现
-    UFUNCTION(BlueprintNativeEvent,BlueprintCallable, Category = "BlueprintFunc")
-    void shoot(FVector StartLocation,FVector EndLocation);
+    //武器射程(对于即时命中武器是最大伤害距离，对于投射物是达到该距离触发伤害效果)
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    float Range;
+    //武器射速(次数/分钟)
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    float RPM;
+    //基础伤害
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    float BasicDamage;
+    //TODO 基础伤害类型  
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    float BasicDamageType;
+    //支持的开火模式
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TSet<FireMode> SupportedFireModes;
+    //弹匣容量
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    int32 Magazine;
+    //装弹时间
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    float ReloadTime;
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<UParticleSystem> GunFireEmitter;
     
-    UFUNCTION(BlueprintNativeEvent,BlueprintCallable, Category = "BlueprintFunc")
-    void shoot2();
-
+    ///////////////////////////////////////////武器升级和自定义/////////////////////////////////////////
+    //枪械模型
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<UStaticMeshComponent> Model;
     //对于直线型武器 返回起点终点 对于抛物线武器返回 矢量发射方向 和 矢量力
     UFUNCTION(BlueprintNativeEvent,BlueprintCallable,Category="BlueprintHook")
     FShootDirectionStruct GetShootDirection();
+    // 枪口粒子发射器
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<UParticleSystemComponent> MuzzleEmitter;
+    // 备用-武器特效粒子
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<UParticleSystemComponent> WeaponVFSEmitter;
+    // 承载武器改装的场景组件
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<USceneComponent> Muzzle;
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<USceneComponent> Rail1;
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<USceneComponent> Rail2;
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<USceneComponent> Rail3;
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<USceneComponent> Rail4;
+    //  安装武器瞄具的场景组件
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    TObjectPtr<USceneComponent> RailSight;
+    //TODO 各类BUFF的实现和结构，这里需要有一个针对改装后武器各种加成的集合，只在改装武器时更新，减少伤害计算时运算量。
+    ////////////////////////////////武器事件//////////////////////////////////////////////////
+    //射击的顶层实现
+    UFUNCTION(BlueprintCallable, Category = "BlueprintFunc")
+    void shoot();
+    UFUNCTION(BlueprintNativeEvent,BlueprintCallable, Category = "BlueprintFunc")
+    void shoot2();
+    ////////////////////////////////RunTime数据//////////////////////////////////////////////////
+    //剩余弹药数量
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    int32 RoundRemain;
+    //下次允许开火的时间
+    UPROPERTY(BlueprintReadWrite,EditAnywhere)
+    int32 AllowShootTime=0;
 protected:
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
-
+    
 public:
     // Called every frame
     virtual void Tick(float DeltaTime) override;
