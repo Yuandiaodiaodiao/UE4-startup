d2ddbbe22e173bac4bb15305f62ffa5dfd174f95
Suriba <SuribaCN@outlook.com>
2021-09-13 00:49:07 +0800

    简单的ai巡逻与攻击逻辑，

--- Source/A/Enum/EnumLibrary.h
+++ Source/A/Enum/EnumLibrary.h
@@ -4,7 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
-#include "enum.generated.h"
+#include "EnumLibrary.generated.h"
+//AI当前状态，如巡逻，索敌，攻击
+UENUM()
+enum EAIBehaviour:uint8
+{
+	Idle   UMETA(DisplayName="空闲"),
+	Patrol   UMETA(DisplayName="巡逻"),
+	//警觉，但不会大幅移动搜索目标
+	Vigilant   UMETA(DisplayName="警戒"),
+	Searching   UMETA(DisplayName="搜寻"),
+	Combat   UMETA(DisplayName="战斗"),
+	Escaping   UMETA(DisplayName="脱离战斗中"),
+	Controlled   UMETA(DisplayName="被控制"),
+};
+
+
 
 /**
  * 
