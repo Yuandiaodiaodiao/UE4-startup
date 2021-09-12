f89e75464900f5bdbb93abbda1ecf1b904a3b9e2
Suriba <SuribaCN@outlook.com>
2021-09-13 00:48:20 +0800

    简单的ai巡逻与攻击逻辑，新增ai状态枚举

---
+++ Source/A/Enum/EnumLibrary.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "enum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class A_API Uenum : public UObject
+{
+	GENERATED_BODY()
+};
